08f4b67e241429c0ccdfbbeb40e0ee6940a20928
tirth <tirth5524@gmail.com>
2024-07-06 09:41:02 +0530

    add login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -106,7 +106,7 @@ const loginUser =asyncHandler(async(req,res)=>{
     //send cookie
     const {email,username,password}=req.body
 
-    if (!username || !email) {
+    if (!(username || email)) {
         throw new ApiError(400,"Username or password is required.")
     }
 
@@ -151,7 +151,32 @@ const loginUser =asyncHandler(async(req,res)=>{
 
 })
 
+
+const logoutUser = asyncHandler(async(req,res)=>{
+
+    await User.findByIdAndUpdate(
+        req.user._id,{
+            $set:{
+                refreshToken:undefined
+            }
+        },{
+            new :true
+        }
+
+    )
+
+    const options = {
+        httpOnly : true,
+        secure:true
+    }
+
+    return res.status(200).
+    clearCookie("accessToken",options).
+    clearCookie("refreshToken",options).json(new ApiResponse(200,{},"User logged Out."))
+})
+
 export  { 
         registerUser ,
         loginUser,
+        logoutUser
 }
