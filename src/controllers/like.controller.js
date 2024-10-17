25cb2b3ed8693a89231adfd42b5958a6ad3be43d
tirth <tirth5524@gmail.com>
2024-10-17 07:40:34 +0530

    add like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,8 +6,27 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
-})
+    const userId = req.user._id
+
+
+    if (!videoId) {
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+ 
+    const existingLike = await Like.findOne({ video: videoId, user: userId });
+
+    if (existingLike) {
+       
+        await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(new ApiResponse(200, "Video unliked successfully"));
+    } else {
+        
+        const newLike = new Like({ video: videoId, user: userId });
+        await newLike.save();
+        return res.status(200).json(new ApiResponse(200, "Video liked successfully"));
+    }
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
